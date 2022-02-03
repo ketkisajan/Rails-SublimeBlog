f1c7b7fd5b0598fdc596b302aa77782e9c2f6ee8
Ketki Sajan <ketki.sajan@IN1121-FD6R6M.local>
2022-02-03 15:50:19 +0530

    Add signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'articles#home'
   get 'about', to: 'articles#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
