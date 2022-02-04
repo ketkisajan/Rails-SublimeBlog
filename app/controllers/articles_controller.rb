a11a52cc5b2226b299f28e23f37a48d3a847926e
Ketki Sajan <ketki.sajan@IN1121-FD6R6M.local>
2022-02-04 16:23:05 +0530

    Login session and admin functionality added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController 
   before_action :set_article, only: [:show,:edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def home 
   end 
 
@@ -12,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def index 
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end 
 
   def new 
@@ -21,7 +24,7 @@ class ArticlesController < ApplicationController
 
   def create 
     @article = Article.new(article_params)
-    @article.user = USer.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to @article
@@ -56,5 +59,12 @@ class ArticlesController < ApplicationController
   def article_params 
     params.require(:article).permit(:title, :description)
   end 
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
 
 end
